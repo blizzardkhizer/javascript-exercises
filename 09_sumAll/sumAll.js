651ae59ee09507d575ea8186d5239104a9af6385
blizzardkhizer <89891484+blizzardkhizer@users.noreply.github.com>
2025-08-27 19:04:22 -0400

    Finished upto 10

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if (a < 1 || b < 1) return "ERROR";
+    if (a > b) {
+        [a, b] = [b, a];
+    }
+    let sum = a;
+    for (let i = a + 1; i <= b; i++) {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
